6f310ab0b6465c5eb26f882dbd35909230efe64a
cheyao <cyao@duck.com>
2026-07-13 20:45:29 +0200

    Successful packet transmission

--- bootloader/bootloader.c
+++ bootloader/bootloader.c
@@ -7,12 +7,12 @@
 #define LED_L PA4
 #define LED_R PB11
 
-#define TOUCH_R1 A9
-#define TOUCH_R0 A8
-#define TOUCH_L0 A7
-#define TOUCH_L1 A5
+#define TOUCH_R1 PA9
+#define TOUCH_R0 PA8
+#define TOUCH_L0 PA7
+#define TOUCH_L1 PA5
 
-__attribute__((naked)) int main() {
+int main() {
 	// Setup PLL
 	SystemInit();
 	funGpioInitAll();
@@ -25,20 +25,23 @@ __attribute__((naked)) int main() {
 
 	// Initialize ports
 	GPIOA->CFGHR &= ~((0xf << (4 * 3)) | (0xf << (4 * 4)));
-	GPIOA->CFGHR |= (GPIO_CNF_IN_PUPD) << (4 * 3) | (GPIO_CNF_IN_PUPD) << (4 * 4);
+	GPIOA->CFGHR |= (GPIO_Speed_2MHz | GPIO_CNF_OUT_PP) << (4 * 3) | (GPIO_Speed_2MHz | GPIO_CNF_OUT_PP) << (4 * 4);
 
 	// Set to pull-down
 	GPIOA->BSHR = (1 << (16 + 11)) | (1 << (16 + 12));
 
 	// 42MHz clock
 	RCC->CFGR0 = (RCC->CFGR0 & ~RCC_USBPRE) | RCC_USBPRE_DIV3;
-	RCC->APB2PCENR |= RCC_AFIOEN;
+	RCC->APB2PCENR |= RCC_AFIOEN | RCC_IOPAEN;
 	RCC->AHBPCENR = RCC_AHBPeriph_SRAM;
 	RCC->APB1PCENR |= RCC_USBEN;
+
 	USBD->CNTR = USBD_FRES; // Suspend & disable all interrupts
 	USBD->CNTR = 0;
 
-	Delay_Us(1000);
+	// Delay a tad
+	for (volatile int i = 0; i < 1000; i++)
+		;
 
 	// Initialize required registers & packet buffer description table
 	USBD->BTABLE = 0;
@@ -47,11 +50,11 @@ __attribute__((naked)) int main() {
 	// EP0 has to be RTX
 	USBD_BDT->EP[0].ADDR_TX = USBD_PBA_BASE + 0x00;
 	USBD_BDT->EP[0].COUNT_TX = 0x0; // Configured on the fly
-	USBD_BDT->EP[0].ADDR_RX = USBD_PBA_BASE + 0x40;
+	USBD_BDT->EP[0].ADDR_RX = USBD_PBA_BASE + 0x00;
 	USBD_BDT->EP[0].COUNT_RX = 0x8400; // 2 blocks of 32 bytes
-	USBD_BDT->EP[1].ADDR_TX = USBD_PBA_BASE + 0x80;
+	USBD_BDT->EP[1].ADDR_TX = USBD_PBA_BASE + 0x40;
 	USBD_BDT->EP[1].COUNT_TX = 0x0;
-	USBD_BDT->EP[2].ADDR_RX = USBD_PBA_BASE + 0xC0;
+	USBD_BDT->EP[2].ADDR_RX = USBD_PBA_BASE + 0x80;
 	USBD_BDT->EP[2].COUNT_RX = 0x8400;
 
 	EXTEN->EXTEN_CTR |= EXTEN_USBD_PU_EN;
@@ -60,8 +63,9 @@ __attribute__((naked)) int main() {
 	NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
 
 	GPIOA->BSHR = (1 << 31);
-	while (1) {
-	}
+
+	while (1)
+		;
 }
 
 void USB_LP_CAN1_RX0_IRQHandler(void) __attribute__((interrupt));
@@ -74,14 +78,14 @@ void USB_LP_CAN1_RX0_IRQHandler(void) {
 		printf("TR: %x\n", ep);
 	}
 
-	if (istr & USBD_PMAOVR) {
-		printf("ERROR! USB overflow\n");
+	if (istr & USBD_RESET) {
+		printf("USB reset\n");
 		USBD->ISTR = ~USBD_PMAOVR;
 	}
 
-	if (istr & USBD_ERR) {
-		printf("ERROR! USB error\n");
-		USBD->ISTR = ~USBD_ERR;
+	if (istr & USBD_PMAOVR) {
+		printf("ERROR! USB overflow\n");
+		USBD->ISTR = ~USBD_PMAOVR;
 	}
 
 	if (istr & USBD_WKUP) {
@@ -90,8 +94,13 @@ void USB_LP_CAN1_RX0_IRQHandler(void) {
 	}
 
 	if (istr & USBD_SUSP) {
-		printf("USB we go eep\n");
+		// printf("USB we go eep\n");
 		USBD->ISTR = ~USBD_WKUP;
+	}
+
+	if (istr & USBD_ESOF) {
+		// printf("USB ESOF\n");
+		USBD->ISTR = ~USBD_ESOF;
 	}
 
 	if (istr & USBD_SOF) {
@@ -99,8 +108,8 @@ void USB_LP_CAN1_RX0_IRQHandler(void) {
 		USBD->ISTR = ~USBD_SOF;
 	}
 
-	if (istr & USBD_ESOF) {
-		printf("USB ESOF\n");
-		USBD->ISTR = ~USBD_ESOF;
+	if (istr & USBD_ERR) {
+		printf("ERROR! USB error\n");
+		USBD->ISTR = ~USBD_ERR;
 	}
 }
--- bootloader/usbd.h
+++ bootloader/usbd.h
@@ -3,17 +3,15 @@
 
 #include "ch32fun.h"
 
-typedef struct 
-{
+typedef struct __attribute__((__packed__)) {
 	__IO uint32_t ADDR_TX;
 	__IO uint32_t COUNT_TX;
 	__IO uint32_t ADDR_RX;
 	__IO uint32_t COUNT_RX;
 } USBD_DESC_TypeDef;
 
-typedef struct 
-{
-    USBD_DESC_TypeDef EP[8];
+typedef struct __attribute__((__packed__)) {
+	USBD_DESC_TypeDef EP[8];
 } USBD_BDT_TypeDef;
 
 // 0x400 of shared memory
@@ -22,7 +20,7 @@ typedef struct
 // Used to 0x80
 // Buffer Description Table
 #define USBD_BDT_BASE 0x40006000
-#define USBD_BDT ((USBD_BDT_TypeDef *) USBD_BDT_BASE)
+#define USBD_BDT ((USBD_BDT_TypeDef*)USBD_BDT_BASE)
 
 // Packet buffer (0x400 - 0x80 = 0x380) left
 #define USBD_PBA_BASE 0x80 // Offset from USBD_BDT_BASE
