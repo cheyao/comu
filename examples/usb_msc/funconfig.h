743221dc33bd67435f6721ff6d9e09a659321558
cheyao <cyao@duck.com>
2026-07-30 14:36:29 +0200

    Add msc example

---
+++ examples/usb_msc/funconfig.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCONFIG_H
+#define _FUNCONFIG_H
+
+#define FUNCONF_USE_DEBUGPRINTF     0 // can only have one printf
+#define FUNCONF_USE_USBPRINTF       1
+#define FUNCONF_ENABLE_HPE          0
+#define FUNCONF_SYSTICK_USE_HCLK    1
+
+#define FUNCONF_DEBUG_HARDFAULT     0
+
+#endif
